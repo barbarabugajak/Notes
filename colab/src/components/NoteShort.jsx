597ade5e3d15c45fa384d1e6f497ef822c2338ae
Coder <basia.bugi.coding@icloud.com>
2024-08-15 16:00:00 +0200

    Fix note.text str length

--- colab/src/components/NoteShort.jsx
+++ colab/src/components/NoteShort.jsx
@@ -2,13 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NoteShort({ note }) {
 
+    const fixString = (str, maxLength) => {
+        if (str.length > maxLength) {
+          return str.slice(0, maxLength) + '...';
+        }
+        return str;
+      };
+
     return (
         <div className="card">
             <div className="card-header">
                 {note.name}
             </div>
             <div className="card-body">
-                <p className="card-text">{note.text}</p>
+                <p className="card-text">{fixString(note.text, 100)}</p>
                 <a href="#" className="btn btn-primary">Edit</a>
             </div>
         </div>
