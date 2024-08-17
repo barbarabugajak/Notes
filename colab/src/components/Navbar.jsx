7401e6a6915c53b9f38ece022838cf15ff0aa375
Coder <basia.bugi.coding@icloud.com>
2024-08-17 17:46:40 +0200

    Create the nav component

---
+++ colab/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar sticky-top bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Notes</a>
+            </div>
+        </nav>
+    )
+}
