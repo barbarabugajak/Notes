130dc6070feace5d414daf073746a6b2caf8e35f
Coder <basia.bugi.coding@icloud.com>
2024-08-21 11:39:23 +0200

    Add visual distinction between private/public

--- colab/src/components/TogglePublicButton.jsx
+++ colab/src/components/TogglePublicButton.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function TogglePublicButton({ noteId, isPublic }) {
-
+    const className = isPublic ? "btn btn-success" : "btn btn-warning";
     return (
         <div>
             <button type="button" 
-                className="btn btn-secondary"
+                className= {className}
                 style={{
                     marginLeft: '1%',
-                    marginTop: '1%'
+                    marginTop: '1%',
                 }}
                 >{isPublic ? "Make Private" : "Make Public"}</button>
         </div>
